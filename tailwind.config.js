1a13b993246d59cf827c83226847a4e002d13044
GabrielOJorge <goliver331@proton.me>
2023-08-14 18:51:12 -0300

    feat: add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'blue-primary': '#1E255E',
+        'blue-secondary': '#3C4EF3',
+      },
+    },
   },
   plugins: [],
-}
-
+};
